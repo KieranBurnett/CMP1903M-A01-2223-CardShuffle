8e73aaf2c3ac4e1ab1c684ad0e5d6d8825aee637
KieranBurnett <kieran.n.burnett@gmail.com>
2023-02-09 17:41:07 +0000

    Inclusion to namespace

--- CMP1903M A01 2223/Testing.cs
+++ CMP1903M A01 2223/Testing.cs
@@ -1,5 +1,13 @@
-﻿class Testing
-    {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+	class Testing
+	{
 		//Creates a pack object
 		//Calls the shuffleCardPack method with each shuffle type
 		//Calls the deal method
