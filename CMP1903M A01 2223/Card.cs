a7d4406bc8d838406b8370dcfe2d249d4f565ecc
KieranBurnett <kieran.n.burnett@gmail.com>
2023-02-17 11:14:34 +0000

    Validation of the sets in the card class

--- CMP1903M A01 2223/Card.cs
+++ CMP1903M A01 2223/Card.cs
@@ -8,19 +8,27 @@ namespace CMP1903M_A01_2223
 {
     class Card
     {
-        //Base for the Card class.
-        //Value: numbers 1 - 13
-        //Suit: numbers 1 - 4
-        //The 'set' methods for these properties could have some validation
-        public int Value { get; set; }
-        public int Suit { get; set; }
-        
-        /*enum suit
+		//Base for the Card class.
+		//Value: numbers 1 - 13
+		//Suit: numbers 1 - 4
+		//The 'set' methods for these properties could have some validation
+		public int Value 
         {
-            Hearts,
-            Spades,
-            Diamond,
-            Clubs,
-        }*/
+            get { return Value; }
+            set
+            {
+                if (1<=value && value <= 13) { Value = value; }
+                else { Console.WriteLine("Card value must be inbetween 1 - 13. "); }
+            }
+        }
+        public int Suit
+        {
+            get { return Suit; }
+            set
+            {
+                if (1 <= value && value <= 4) { Suit = value; }
+                else { Console.WriteLine("Card suit must be inbetween 1 - 4"); }
+            }
+        }
     }
 }
