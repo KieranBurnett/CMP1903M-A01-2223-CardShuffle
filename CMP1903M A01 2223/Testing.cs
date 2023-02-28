6b6572b3fa86526947d9f5a3d3f069b068df0a78
KieranBurnett <kieran.n.burnett@gmail.com>
2023-02-28 14:38:34 +0000

    Implementation of "Deal multiple Cards"

--- CMP1903M A01 2223/Testing.cs
+++ CMP1903M A01 2223/Testing.cs
@@ -17,15 +17,20 @@ namespace CMP1903M_A01_2223
 			if (Pack.shuffleCardPack(2)) { Console.WriteLine("Successfully shuffled via Riffle Shuffle"); }
 			if (Pack.shuffleCardPack(3)) { Console.WriteLine("No Shuffle complete"); }
 
+			Console.WriteLine("");
 			//Calls the deal method
 			Card dealt_card = Pack.deal();
 			//Check what is returned
-			Console.WriteLine($"Suit: {dealt_card.card_suit}, Value: {dealt_card.card_value}");
+			Console.WriteLine($"Dealt Card's Suit: {dealt_card.card_suit}, Dealt Card's Value: {dealt_card.card_value}");
 
+			Console.WriteLine("");
 			//Calls the deal mulltiple method
-			
+			List<Card> cards = Pack.dealCard(5);
 			//Check what is returned
-			
+			for (int i = 0; i < 5; i++)
+			{
+				Console.WriteLine($"Card number {i}'s Suit: {cards[i].card_suit}, Card number {i}'s Value: {cards[i].card_value}");
+			}
 		}
 	}
 }
